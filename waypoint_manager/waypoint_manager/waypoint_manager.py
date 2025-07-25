d39d29c700dfa86f23c399e56b7452455fbb2e4b
emilnovak <emil.tibor.novak@gmail.com>
2025-07-25 18:38:17 +0000

    Add append implementation

--- waypoint_manager/waypoint_manager/waypoint_manager.py
+++ waypoint_manager/waypoint_manager/waypoint_manager.py
@@ -1,5 +1,7 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.duration import Duration
+from tf2_ros import Buffer, TransformListener, TransformException
 
 from waypoint_msgs.msg import *
 from waypoint_msgs.srv import *
@@ -8,6 +10,15 @@ from waypoint_msgs.srv import *
 class WaypointManager(Node):
     def __init__(self):
         super().__init__('waypoint_manager')
+
+        self.map_link_name = 'fastbot_1_odom'
+        self.base_link_name = 'fastbot_1_base_link'
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
+        # initialize waypoints
+        self.waypoints = []
 
         self.append_srv = self.create_service(AppendWaypoint, 'waypoint/append', self.append_cb)
         self.delete_srv = self.create_service(DeleteWaypoint, 'waypoint/delete', self.delete_cb)
@@ -21,8 +32,15 @@ class WaypointManager(Node):
     def append_cb(self, request, response):
         self.get_logger().info('append called')
 
-        response.success = False
-        response.message = 'Missing implementation'
+        try:
+            pose = self.tf_buffer.lookup_transform(self.map_link_name, self.base_link_name, rclpy.time.Time(), timeout=Duration(seconds=0.1))
+        except TransformException as e:
+            response.success = False
+            response.message = f'Tranform exception: {e}'
+            return response
+
+        self.waypoints.append(pose.transform)
+        response.success = True
         return response
 
     def delete_cb(self, request, response):
