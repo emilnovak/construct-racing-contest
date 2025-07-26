61348e2f85bf56b2b3acd19412ce7c84bcc966db
emilnovak <emil.tibor.novak@gmail.com>
2025-07-26 17:47:58 +0000

    Add dev mode to launch

--- fastbot_racing/launch/start_racing.launch.py
+++ fastbot_racing/launch/start_racing.launch.py
@@ -1,9 +1,20 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    dev_mode = LaunchConfiguration('dev')
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'dev',
+            default_value='false',
+            description='Launch in development mode with RViz'
+        ),
+
         Node(
             package='waypoint_manager',
             executable='waypoint_manager',
@@ -16,6 +27,16 @@ def generate_launch_description():
             executable='racetrack_controller',
             name='racetrack_controller',
             output='screen',
-            parameters=[{'use_sim_time': False}]
+            parameters=[{'use_sim_time': False,
+                        'start_paused': dev_mode,
+            }]
         ),
+        Node(
+            condition=IfCondition(dev_mode),
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', '/home/user/ros2_ws/src/construct-racing-contest/fastbot_racing/fastbot_racing/config/developer_conf.rviz']
+        )
     ])
